1b4cfa7761b4391fe9000cdc060e6e7c35f4ef20
Adriano Sela Aviles <adriano.selaviles@gmail.com>
2017-03-24 23:29:19 -0700

    lol

--- getbutton.js
+++ getbutton.js
@@ -1,4 +1,6 @@
 <script type = "text/javascript">
-	var username = document.forms[0].elements[0]
-	var password = document.forms[0].elements[1]
+	var username = document.forms[0].elements[0];
+	var password = document.forms[0].elements[1];
+	document.write(username);
 </script>
+
